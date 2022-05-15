8562135c3630203c199a62f06e935dea027951c1
GoranAviani <goranaviani@gmail.com>
2022-05-15 14:17:41 +0200

    how_to_test_in_react

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 
 import Button from "./components/Button";
 import Input from "./components/Input";
+import {useState} from "react";
 
 function App() {
+    const [showDiv, setShowDiv] = useState(true)
     return (
         <div className="App">
             This is app
-            <Input/>
+            <Input showDiv={showDiv}/>
             <Button/>
         </div>
     );
