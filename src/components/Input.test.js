94d1083e63a85bcfdbbf3ffc261cd2d955efb050
GoranAviani <goranaviani@gmail.com>
2022-05-15 14:16:38 +0200

    how_to_test_in_react

--- src/components/Input.test.js
+++ src/components/Input.test.js
@@ -4,7 +4,7 @@ import Input from "./Input";
 describe("input component test", ()=>{
 
     it("rendered input", ()=>{
-        const {getByTestId} = render(<Input/>)
+        const {getByTestId} = render(<Input showDiv={false}/>)
         const input = getByTestId("search-bar")
         expect(input).toBeInTheDocument()
 
