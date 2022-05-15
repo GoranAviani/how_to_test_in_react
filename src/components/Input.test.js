35c6f6e2804f595bc96a645f9e2d43a31153afe5
GoranAviani <goranaviani@gmail.com>
2022-05-15 14:45:27 +0200

    how_to_test_in_react

--- src/components/Input.test.js
+++ src/components/Input.test.js
@@ -1,4 +1,4 @@
-import {getByTestId, queryByTestId, render} from '@testing-library/react'
+import {getByTestId, queryByTestId, render, fireEvent, act} from '@testing-library/react'
 import Input from "./Input";
 
 describe("input component test", () => {
@@ -21,6 +21,22 @@ describe("input component test", () => {
         const {queryByTestId} = render ( <Input showDiv={false}/> )
         const greetingText = queryByTestId("greeting-test")
         expect(greetingText).toBeFalsy()
+    })
+
+
+    it("change on input causes change on header", ()=> {
+            const {getByTestId} = render(<Input showDiv={false} /> )
+            const input = getByTestId("search-bar")
+                    const header = getByTestId("display-search")
+
+            const userInput = "Oliver"
+            fireEvent.change(input, {target: {value: userInput}})
+
+            expect(header.innerHtml).toBe(userInput)
+
+
+
+
     })
 
 })
