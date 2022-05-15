db2f5885f34cda49d90beeaa6b999c5fb6a2ea46
GoranAviani <goranaviani@gmail.com>
2022-05-15 14:45:24 +0200

    how_to_test_in_react

--- src/components/Input.js
+++ src/components/Input.js
@@ -11,7 +11,7 @@ const Input = ({showDiv}) => {
                 setUserInput(e.target.value)
             }}/>
             <br/>
-            <h2 data-test-id="display-seaerch">{userInput}</h2>
+            <h2 data-testid="display-search">{userInput}</h2>
             <br/>
             {showDiv && (
                 <div data-testid="greeting-test">How to test in react</div>
