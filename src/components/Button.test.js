58020ca066e5c6202c4c82fb28c6bd65e5d7b221
GoranAviani <goranaviani@gmail.com>
2022-05-15 15:10:51 +0200

    how_to_test_in_react

---
+++ src/components/Button.test.js
@@ -0,0 +1,13 @@
+import {getByTestId, queryByTestId, render, fireEvent, act} from '@testing-library/react'
+import Button from "./Button";
+
+describe("button component test", () => {
+
+    it("rendered button", () => {
+        const {getByTestId} = render(<Input showDiv={false}/>)
+        const input = getByTestId("button")
+        expect(input).toBeTruthy()
+
+    })
+
+)
