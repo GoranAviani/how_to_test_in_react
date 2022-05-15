fd4dc6ffeefbeefdaf186cd0a4f7e00ed7f3ccef
GoranAviani <goranaviani@gmail.com>
2022-05-15 13:31:06 +0200

    how_to_test_in_react

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 
 import Button from "./components/Button";
 import Input from "./components/Input";
+
 function App() {
-  return (
-    <div className="App">
-        <Input/>
-      <Button/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Input/>
+            <Button/>
+        </div>
+    );
 }
 
 export default App;
