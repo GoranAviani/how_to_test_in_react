177c72e509467830cd5bb2848fe69846f8cda2c0
GoranAviani <goranaviani@gmail.com>
2022-05-15 13:45:32 +0200

    how_to_test_in_react

--- src/components/Input.test.js
+++ src/components/Input.test.js
@@ -6,7 +6,7 @@ describe("input component test", ()=>{
     it("rendered input", ()=>{
         const {getByTestId} = render(<Input/>)
         const input = getByTestId("search-bar")
-        expect(input).toBeTruthy()
+        expect(input).toBeInTheDocument()
 
     })
 
